20e76101ecbfa2e92125caeba05cb43aa3e32270
Khushi <dpskhu13108@gmail.com>
2020-12-29 16:04:40 +0530

    nav

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -1,15 +1,16 @@
 import React from "react"
 import Link from "next/link"
+import Typography from '@material-ui/core/Typography';
 import { Nav, PageBody } from "./styles.js"
 
 const Navbar = ({ children }) => {
   return (
     <React.Fragment>
       <Nav>
-        <p><b>
-          Periodo
-        </b>
-        </p>
+      <Typography variant="h5" >
+                Period Tracker
+        </Typography>
+      
         <Link href="/">
           <a>Home</a>
         </Link>
