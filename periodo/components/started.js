dc0f1e1ffa5f043ad1f8c0fcb60a0dac4b821cd9
Khushi <dpskhu13108@gmail.com>
2021-01-04 22:54:27 +0530

    added next date of period

--- periodo/components/started.js
+++ periodo/components/started.js
@@ -13,8 +13,26 @@ const Start=()=>{
         return (`${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`)
         }
 
+        function getnextDate(separator=''){
+
+            let newDate = new Date()
+            let date = newDate.getDate()+28;
+            let month = newDate.getMonth() + 1;
+            if(date>30){
+                month = (newDate.getMonth()%12) + 2;
+            }
+            let year = newDate.getFullYear();
+            if(month>12){
+                year = newDate.getFullYear() + 1;
+            }
+            
+            return (`${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date%30}`)
+            }
+
     return(
         <div>
+            <h4 className={styles.heading}>Period Cycle: 28 days</h4>
+            <h4 className={styles.heading}>Next Period Date: {getnextDate('-')}</h4>
             <h4 className={styles.heading}>Period just started: {getCurrentDate('-')}</h4>
             <Link href='/calender'>
             <a className={styles.heading}>Tap here</a>
