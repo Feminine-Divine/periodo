45c7f69bf879f55d58479708fc3581a7ed3ac3d1
Khushi <dpskhu13108@gmail.com>
2021-01-04 17:41:02 +0530

    added today's date

--- periodo/components/started.js
+++ periodo/components/started.js
@@ -3,9 +3,19 @@ import Link from 'next/link'
 import styles from './started.module.css'
 
 const Start=()=>{
+    function getCurrentDate(separator=''){
+
+        let newDate = new Date()
+        let date = newDate.getDate();
+        let month = newDate.getMonth() + 1;
+        let year = newDate.getFullYear();
+        
+        return (`${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`)
+        }
+
     return(
         <div>
-            <h4 className={styles.heading}>Period just started:</h4>
+            <h4 className={styles.heading}>Period just started: {getCurrentDate('-')}</h4>
             <Link href='/calender'>
             <a className={styles.heading}>Tap here</a>
             </Link>
