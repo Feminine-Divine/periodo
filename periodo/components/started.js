a8f35c9ddaf76d5a6cbfd28f0636610896f99f98
Khushi <dpskhu13108@gmail.com>
2021-01-04 16:07:56 +0530

    linked page

---
+++ periodo/components/started.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Start=()=>{
+    return(
+        <div>
+            <Link href='/calender'>
+            <a>Period just started:</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Start
---
+++ periodo/pages/calender.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../components/calender/calender"
+import Navbar from "../components/navbar"
+
+const calender = ()=>{
+    return (
+        <>
+    <Navbar/>
+    <Home/>
+    </>
+    )
+}
+
+export default calender
