d10fcf7cfe238ed185b2dce915b9e5b9b0e6e9f5
Khushi <dpskhu13108@gmail.com>
2021-01-11 23:49:59 +0530

    added more

--- periodo/components/flow/Flow.js
+++ periodo/components/flow/Flow.js
@@ -6,6 +6,7 @@ import style from "./Flow.module.css"
 const Flow=()=>{
 
     return(
+        <>
         <div className={style.container}>
         <p className={style.heading}>Blood Flow:</p>
         <StarRatingComponent 
@@ -16,6 +17,28 @@ const Flow=()=>{
           emptyStarColor={`white`}
         />
       </div>
+      <div className={style.container}>
+        <p className={style.heading}>Period Pain:</p>
+        <StarRatingComponent 
+          name="rate1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+        />
+      </div>
+      <div className={style.container}>
+        <p className={style.heading}>Mood:</p>
+        <StarRatingComponent 
+          name="rate1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+      
+      </>
     )
 }
 
