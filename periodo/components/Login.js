d73b6b4d0761dfe46a00838a184c400dab76c6f0
Khushi <dpskhu13108@gmail.com>
2021-03-03 19:33:50 +0530

    updated

--- periodo/components/Login.js
+++ periodo/components/Login.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Link from 'next/link'
 import { GoogleLogin } from "react-google-login";
+import { refreshTokenSetup } from "../utils/refreshToken";
 
 const clientID = '246533887386-o890hff552huvands3abmla9p72h2o18.apps.googleusercontent.com';
 
 function Login() {
   const onSuccess = (res) => {
-    console.log('[Login Success] currentUser:', res.profileOBj);
+    console.log('[Login Success] currentUser:', res.profileObj);
   };
 
   const onFailure = (res)=> {
@@ -15,6 +17,7 @@ function Login() {
   return (
     <>
       <div>
+      <Link href='/Homepage'>
         <GoogleLogin
          clientId={clientID}
          buttonText="Login"
@@ -23,7 +26,8 @@ function Login() {
          cookiePolicy={'single_host_origin'}
          style={{ marginTop: "100px"}}
          isSignedIn={true}
-        />
+         />
+      </Link>
       </div>
     </>
   );
