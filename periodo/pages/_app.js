138587841880a221b043151e23c6486251e6c62a
Khushi <dpskhu13108@gmail.com>
2021-03-07 12:11:02 +0530

    update the project

--- periodo/pages/_app.js
+++ periodo/pages/_app.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import "../styles/globals.css"
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
---
+++ periodo/pages/api/login.js
@@ -0,0 +1,11 @@
+
+import auth0 from '../utils/auth0';
+
+export default async function login(req, res) {
+  try {
+    await auth0.handleLogin(req, res, {});
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+}
