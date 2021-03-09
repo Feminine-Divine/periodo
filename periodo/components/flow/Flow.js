7b778f3ca1a68afb10acbd758fbdafc789450d24
Anushka Chitranshi <52864294+codebuzzer01@users.noreply.github.com>
2021-03-10 02:31:30 +0530

    Adding more ratings.
    More ratings are being added in flow.js.

--- periodo/components/flow/Flow.js
+++ periodo/components/flow/Flow.js
@@ -37,7 +37,126 @@ const Flow=()=>{
           emptyStarColor={`white`}
           />
       </div>
-      
+        <div className={style.container}>
+        <p className={style.heading}>Weakness:</p>
+        <StarRatingComponent 
+          name="weak1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+        <div className={style.container}>
+        <p className={style.heading}>Breast Tenderness:</p>
+        <StarRatingComponent 
+          name="breast1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+        <div className={style.container}>
+        <p className={style.heading}>Acne:</p>
+        <StarRatingComponent 
+          name="acne1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+        <div className={style.container}>
+        <p className={style.heading}>Cravings:</p>
+        <StarRatingComponent 
+          name="food1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+      <div className={style.container}>
+        <p className={style.heading}>Duration( No. of Days ):</p>
+        <StarRatingComponent 
+          name="days1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+     <div className={style.container}>
+        <p className={style.heading}>Bloating:</p>
+        <StarRatingComponent 
+          name="bloat1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+    <div className={style.container}>
+        <p className={style.heading}>Fever:</p>
+        <StarRatingComponent 
+          name="fever1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+      <div className={style.container}>
+        <p className={style.heading}>HairLoss:</p>
+        <StarRatingComponent 
+          name="hair1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+     <div className={style.container}>
+        <p className={style.heading}>Migranes:</p>
+        <StarRatingComponent 
+          name="head1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+    <div className={style.container}>
+        <p className={style.heading}>Joint Pain:</p>
+        <StarRatingComponent 
+          name="pain1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+    <div className={style.container}>
+        <p className={style.heading}>Vomiting:</p>
+        <StarRatingComponent 
+          name="vomit1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
+     <div className={style.container}>
+        <p className={style.heading}>Weight Changes:</p>
+        <StarRatingComponent 
+          name="weight1"
+          val="1"
+          starCount={5}
+          starColor= {`yellow`}
+          emptyStarColor={`white`}
+          />
+      </div>
       </>
     )
 }
