b2445424da883133864c9cd61134cfc89a110948
mdPial <root.pial@gmail.com>
2021-03-10 13:21:14 +0600

    Add navbar

--- periodo/pages/flow.js
+++ periodo/pages/flow.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Flow from "../components/flow/Flow"
+import Navbar from "../components/navbar"
 
 const flow=()=>{
     return(
-        <Flow/>
+        <>
+            <Navbar/>
+            <Flow/>
+        </>
     )
 }
 export default flow
