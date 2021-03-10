d28f03106bc3ec74dadc5af5e4b45f1a8091f865
Nishi Agrawal <nishihere19@gmail.com>
2021-03-10 22:46:59 +0530

    Third-party authentication added

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -2,8 +2,10 @@ import React from "react"
 import Link from "next/link"
 import Typography from '@material-ui/core/Typography';
 import { Nav, PageBody } from "./styles.js"
+import { useUser } from '@auth0/nextjs-auth0';
 
 const Navbar = ({ children }) => {
+  const {user} = useUser();
   return (
     <React.Fragment>
       <Nav>
@@ -14,6 +16,12 @@ const Navbar = ({ children }) => {
         <Link href="/">
           <a>Home</a>
         </Link>
+    {user?<Link href="/api/auth/logout">
+          <a>Logout</a>
+        </Link> : <Link href="/api/auth/login">
+          <a>Login</a>
+        </Link>}
+        
       </Nav>
       <PageBody>{children}</PageBody>
     </React.Fragment>
