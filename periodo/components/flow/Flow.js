710b284712f156d2db9d5ee17d780d9a2d8ef52d
Sayna Parida <saynaparida33@gmail.com>
2021-03-11 19:12:45 +0530

    Styled the rating labels

--- periodo/components/flow/Flow.js
+++ periodo/components/flow/Flow.js
@@ -1,46 +1,54 @@
 import React from "react"
 import ReactDOM from 'react-dom';
 import StarRatingComponent from 'react-star-rating-component';
+import BeautyStars from 'beauty-stars';
 import style from "./Flow.module.css"
 
-const Flow=()=>{
-
-    return(
-        <>
+export default class App extends React.Component {
+  state = { 
+  value1: 0,
+  value2: 0,
+  value3: 0,
+ };
+  colour = "#ffffff";
+  size = "18px";
+  render() {
+    return (
+      <>
         <div className={style.container}>
-        <p className={style.heading}>Blood Flow:</p>
-        <StarRatingComponent 
-          name="rate1"
-          val="1"
-          starCount={5}
-          starColor= {`yellow`}
-          emptyStarColor={`white`}
-        />
-      </div>
-      <div className={style.container}>
-        <p className={style.heading}>Period Pain:</p>
-        <StarRatingComponent 
-          name="base1"
-          val="1"
-          starCount={5}
-          starColor= {`yellow`}
-          emptyStarColor={`white`}
-        />
-      </div>
-      <div className={style.container}>
-        <p className={style.heading}>Mood:</p>
-        <StarRatingComponent 
-          name="mood1"
-          val="1"
-          starCount={5}
-          starColor= {`yellow`}
-          emptyStarColor={`white`}
+          <p className={style.heading}>Blood Flow:</p>
+          <div className={style.stars}>
+          <BeautyStars
+            value={this.state.value1}
+            onChange={value => this.setState({value1: value })}
+            inactiveColor={this.colour}
+            size={this.size}
           />
-      </div>
-      
+          </div>
+        </div>
+        <div className={style.container}>
+          <p className={style.heading}>Period Pain:</p>
+          <div className={style.stars}>
+          <BeautyStars
+            value={this.state.value2}
+            onChange={value => this.setState({ value2: value })}
+            inactiveColor={this.colour}
+            size={this.size}
+          />
+          </div>
+        </div>
+        <div className={style.container}>
+          <p className={style.heading}>Mood:</p>
+          <div className={style.stars}>
+          <BeautyStars
+            value={this.state.value3}
+            onChange={value => this.setState({ value3: value })}
+            inactiveColor={this.colour}
+            size={this.size}
+          />
+          </div>
+        </div>
       </>
-    )
+    );
+  }
 }
-
-export default Flow
-
