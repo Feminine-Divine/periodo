8286884c802207103309a75ae9a68535737f3822
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-11 23:37:57 +0530

    pages added

--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Navbar from "../components/navbar.js";
-import Home from "../components/calender/calender.js"
-import Start from "../components/started.js"
+import Home from "../components/calender/calender.js";
+import Start from "../components/started.js";
 
-const Homepage =()=> {
+
+const Homepage = () => {
   return (
     <>
       <Navbar />
-      <Start/>
-      </>
+      <Start />
+      
+    </>
   );
-}
+};
 
 export default Homepage;
--- periodo/pages/index.js
+++ periodo/pages/index.js
@@ -1,11 +1,12 @@
   
 import React from "react";
 import Homepage from "./homepage"
-
+import Footer from "../components/Footer/Footer";
 const index =()=> {
   return (
     <>
       <Homepage />
+      <Footer />
       </>
   );
 }
