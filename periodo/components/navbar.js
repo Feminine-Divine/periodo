2d1cd537003a4b2ba3e6896dbee0ff879b09d59d
Aastha Nautiyal <78968859+AasthaNautiyal@users.noreply.github.com>
2021-03-22 17:57:29 +0530

    Updated Gynac Help changes (#116)
    * Updated Gynac Help changes
    
    * fixes #issue76

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -10,12 +10,11 @@ const Navbar = ({ children }) => {
 <React.Fragment>
  
   <Header>
-  <img src="Logo.png" height="64px" width="190px" padding="10px" class="nav-img"></img>
+  <img src="Logo.png" height="64px" width="160px" padding="10px" class="nav-img"></img>
   <Typography variant="h5" >
   </Typography>
         <Nav>
-          <input type= "checkbox" id="nav-btn"></input>
-          
+          <input type= "checkbox" id="nav-btn"></input> 
          <div class="main-navbar"> 
             <Link href="/">
             <a class="Nav-bar Nav-Active">Home</a>
@@ -23,20 +22,20 @@ const Navbar = ({ children }) => {
           <Link href="/tips">
             <a class="Nav-bar">Tips</a>
           </Link>
+          <Link href="/Gynac">
+            <a class="Nav-bar">GynacHelp</a>
+          </Link>
           <Link href = "/analysis">
         <a class="Nav-bar">Quiz</a>
         </Link>
-
-
         <Link href="/calender">
           <a class="Nav-bar">Calendar</a>
         </Link>
         <Link href="/about">
           <a class="Nav-bar">AboutUs</a>
         </Link>
-        
         <Link href="/notes">
-          <a>Notes</a>
+        <a class="Nav-bar">Notes</a>
         </Link>
           </div>
         </Nav>
