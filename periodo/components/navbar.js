425bd122526f1e8e6f04603835f955d8b1340a4b
bhavesh-chandrakar <bhavesh1334@gmail.com>
2021-03-23 15:44:29 +0530

    Fixed Toggle Option Visibility
    Fixes #123

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -1,43 +1,45 @@
-import React from "react";
-import Link from "next/link";
-import { Header, Nav, PageBody } from "./styles.js";
+/** @format */
+
+import React from 'react';
+import Link from 'next/link';
+// import 'navbar.css';
+import { Header, Nav, PageBody } from './styles.js';
 
 const Navbar = ({ children }) => {
   return (
-<React.Fragment>
- 
-  <Header>
-  <img src="Logo.png" height="64px" width="160px" padding="10px" class="nav-img"></img>
+    <React.Fragment>
+      <Header>
+        <img src='Logo.png' height='64px' width='160px' padding='10px' class='nav-img'></img>
         <Nav>
-          <input type= "checkbox" id="nav-btn"></input> 
-         <div class="main-navbar"> 
-            <Link href="/">
-            <a class="Nav-bar Nav-Active">Home</a>
-          </Link>
-          <Link href="/tips">
-            <a class="Nav-bar">Tips</a>
-          </Link>
-          <Link href="/Gynac">
-            <a class="Nav-bar">GynacHelp</a>
-          </Link>
-          <Link href = "/analysis">
-        <a class="Nav-bar">Quiz</a>
-        </Link>
-        <Link href="/calender">
-          <a class="Nav-bar">Calendar</a>
-        </Link>
-        <Link href="/about">
-          <a class="Nav-bar">AboutUs</a>
-        </Link>
-        <Link href="/notes">
-        <a class="Nav-bar">Notes</a>
-        </Link>
+          <input type='checkbox' id='nav-btn'></input>
+          <div class='main-navbar'>
+            <Link href='/'>
+              <a class='Nav-bar Nav-Active'>Home</a>
+            </Link>
+            <Link href='/tips'>
+              <a class='Nav-bar'>Tips</a>
+            </Link>
+            <Link href='/Gynac'>
+              <a class='Nav-bar'>GynacHelp</a>
+            </Link>
+            <Link href='/analysis'>
+              <a class='Nav-bar'>Quiz</a>
+            </Link>
+            <Link href='/calender'>
+              <a class='Nav-bar'>Calendar</a>
+            </Link>
+            <Link href='/about'>
+              <a class='Nav-bar'>AboutUs</a>
+            </Link>
+            <Link href='/notes'>
+              <a class='Nav-bar'>Notes</a>
+            </Link>
           </div>
-          <label for="nav-btn">
-            <div class="toggle">
-              <div class="toggel-btn"></div>
-              <div class="toggel-btn"></div>
-              <div class="toggel-btn"></div>
+          <label for='nav-btn'>
+            <div class='toggle'>
+              <div class='toggel-btn'></div>
+              <div class='toggel-btn'></div>
+              <div class='toggel-btn'></div>
             </div>
           </label>
         </Nav>
--- periodo/components/styles.js
+++ periodo/components/styles.js
@@ -1,21 +1,23 @@
-import styled from "@emotion/styled"
+/** @format */
 
-export const Header = styled("div")`
-width:100%;
-color:white;
-background-color:black;
-display:flex;
-justify-content: space-between;
-align-items: center;
-`
-export const Nav = styled("div")
-`
-width:auto;
-`
+import styled from '@emotion/styled';
 
+export const Header = styled('div')`
+  width: 100%;
+  color: white;
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Nav = styled('div')`
+  width: auto;
+  height: 70px;
+  padding: 30px;
+`;
 
-export const PageBody = styled("div")`
+export const PageBody = styled('div')`
   width: 100%;
   height: 100%;
   padding: 2em;
-  `
+`;
