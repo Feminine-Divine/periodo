6526ac86345644e9fdf4fc50f71edb6baa39a2fb
Nishi Agrawal <nishihere19@gmail.com>
2021-03-23 11:39:43 +0530

    minor errors resolved in nav.js

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -5,62 +5,49 @@ import { useUser } from '@auth0/nextjs-auth0';
 import { Header, Nav, PageBody } from "./styles.js";
 
 const Navbar = ({ children }) => {
-  const {user} = useUser();
+  const { user } = useUser();
   return (
-
-
-<React.Fragment>
- 
-  <Header>
-  <img src="Logo.png" height="64px" width="190px" padding="10px" class="nav-img"></img>
-  <Typography variant="h5" >
-  </Typography>
+    <React.Fragment>
+      <Header>
+        <img src="Logo.png" height="64px" width="190px" padding="10px" class="nav-img"></img>
+        <Typography variant="h5" >
+        </Typography>
         <Nav>
-          <input type= "checkbox" id="nav-btn"></input>
-          
-         <div class="main-navbar"> 
+          <input type="checkbox" id="nav-btn"></input>
+          <div class="main-navbar">
             <Link href="/">
-            <a class="Nav-bar Nav-Active">Home</a>
-          </Link>
-          <Link href="/tips">
-            <a class="Nav-bar">Tips</a>
-          </Link>
-          <Link href = "/analysis">
-        <a class="Nav-bar">Quiz</a>
-        </Link>
-
-    {user?<Link href="/api/auth/logout">
-          <a>Logout</a>
-        </Link> : <Link href="/api/auth/login">
-          <a>Login</a>
-        </Link>}
-        
-        <Link href="/tips">
-          <a>Tips</a>
-
-        <Link href="/calender">
-          <a class="Nav-bar">Calendar</a>
-        </Link>
-        <Link href="/about">
-          <a class="Nav-bar">AboutUs</a>
-
-        </Link>
-        
-        <Link href="/notes">
-          <a>Notes</a>
-        </Link>
+              <a class="Nav-bar Nav-Active">Home</a>
+            </Link>
+            <Link href="/tips">
+              <a class="Nav-bar">Tips</a>
+            </Link>
+            <Link href="/analysis">
+              <a class="Nav-bar">Quiz</a>
+            </Link>
+            {user ? <Link href="/api/auth/logout">
+              <a class="Nav-bar">Logout</a>
+            </Link> : <Link href="/api/auth/login">
+              <a class="Nav-bar">Login</a>
+            </Link>}
+            <Link href="/calender">
+              <a class="Nav-bar">Calendar</a>
+            </Link>
+            <Link href="/about">
+              <a class="Nav-bar">AboutUs</a>
+            </Link>
+            <Link href="/notes">
+              <a class="Nav-bar">Notes</a>
+            </Link>
+            <label for="nav-btn">
+              <div class="toggle" >
+                <div class="toggel-btn"></div>
+                <div class="toggel-btn"></div>
+                <div class="toggel-btn"></div>
+              </div>
+            </label>
           </div>
         </Nav>
-        <label for="nav-btn">
-       <div class="toggle" > 
-         <div class="toggel-btn"></div>
-         <div class="toggel-btn"></div>
-         <div class="toggel-btn"></div>
-       </div>
-       </label>
       </Header>
-
-
       <PageBody>{children}</PageBody>
     </React.Fragment>
   )
