20586065c78aa03f65a6f507b5dcf795f15e3dca
A-HK <kahani.0142@gmail.com>
2021-03-23 18:07:05 +0530

    sticky-nav

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -2,10 +2,12 @@ import React from "react"
 import Link from "next/link"
 import Typography from '@material-ui/core/Typography';
 import { Nav, PageBody } from "./styles.js"
+import styles from "./navbar.module.css"
 
 const Navbar = ({ children }) => {
   return (
     <React.Fragment>
+    <header className={styles.mobileNav}>
       <Nav>
       <img src="Logo.png" height="64px" width="190px" padding="10px"></img>
       <Typography variant="h5" >
@@ -24,6 +26,7 @@ const Navbar = ({ children }) => {
 
       </Nav>
       <PageBody>{children}</PageBody>
+      </header>
     </React.Fragment>
   )
 }
