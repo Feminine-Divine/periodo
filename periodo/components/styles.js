f339c9ab9b994dfce76067d81c52e746aa09b4ad
Bhavesh Chandrakar <50066490+bhavesh1334@users.noreply.github.com>
2021-03-25 21:53:21 +0530

    Update styles.js

--- periodo/components/styles.js
+++ periodo/components/styles.js
@@ -1,23 +1,21 @@
 import styled from "@emotion/styled"
 
 export const Header = styled("div")`
- width: 100%;
- color: white;
- background-color: black;
- display: flex;
- justify-content: space-between;
- align-items: center;
+width:100%;
+color:white;
+background-color:black;
+display:flex;
+justify-content:space-between;
+align-items:center;
 `
-export const Nav = styled("div")
-`
+export const Nav = styled("div")`
  width: auto;
  height: 70px;
  padding: 30px;
 `;
 
 export const PageBody = styled("div")
-`
- width: 100%;
+`width: 100%;
  height: 100%;
  padding: 2em;
 `
