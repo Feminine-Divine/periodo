c5fd1fbbe946bb30c6b637dcd37ae79395c07569
Nishi Agrawal <63252119+nishihere19@users.noreply.github.com>
2021-03-25 15:19:40 +0530

    Third-party authentication added using nextjs-auth0 Issue #16 (#47)
    * Third-party authentication added
    
    * code updated.
    
    * minor errors resolved in nav.js
    
    * moved auth0.js out of pages folder due to nextjs version 9 updates
    
    Co-authored-by: Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
    Co-authored-by: Khushi <dpskhu13108@gmail.com>

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -1,51 +1,58 @@
-import React from "react";
-import Link from "next/link";
+import React from "react"
+import Link from "next/link"
+import Typography from '@material-ui/core/Typography';
+import { useUser } from '@auth0/nextjs-auth0';
 import { Header, Nav, PageBody } from "./styles.js";
 import styles from "./navbar.module.css";
 
 const Navbar = ({ children }) => {
+  const { user } = useUser();
   return (
 <React.Fragment>
         <header className={styles.mobileNav}>
   <Header>
 
-        <img src="Logo.png" height="64px" width="160px" padding="10px" class="nav-img"></img>
+        <img src="Logo.png" height="64px" width="160px" padding="10px" className="nav-img"></img>
         <Nav>
           <input type= "checkbox" id="nav-btn"></input>
-         <div class="main-navbar">
+         <div className="main-navbar">
             <Link href="/">
-            <a class="Nav-bar Nav-Active">Home</a>
-          </Link>
-          <Link href="/tips">
-            <a class="Nav-bar">Tips</a>
-          </Link>
-          <Link href="/Gynac">
-            <a class="Nav-bar">GynacHelp</a>
-          </Link>
-          <Link href = "/analysis">
-        <a class="Nav-bar">Quiz</a>
-        </Link>
-        <Link href="/calender">
-          <a class="Nav-bar">Calendar</a>
-        </Link>
-        <Link href="/about">
-          <a class="Nav-bar">AboutUs</a>
-        </Link>
-        <Link href="/notes">
-        <a class="Nav-bar">Notes</a>
-        </Link>
+              <a className="Nav-bar Nav-Active">Home</a>
+            </Link>
+            {user ? <Link href="/api/auth/logout">
+              <a className="Nav-bar">Logout</a>
+            </Link> : <Link href="/api/auth/login">
+              <a className="Nav-bar">Login</a>
+            </Link>}
+            <Link href="/tips">
+              <a className="Nav-bar">Tips</a>
+            </Link>
+            <Link href="/Gynac">
+              <a className="Nav-bar">GynacHelp</a>
+            </Link>
+            <Link href="/analysis">
+              <a className="Nav-bar">Quiz</a>
+            </Link>
+            <Link href="/calender">
+              <a className="Nav-bar">Calendar</a>
+            </Link>
+            <Link href="/about">
+              <a className="Nav-bar">AboutUs</a>
+            </Link>
+            <Link href="/notes">
+              <a className="Nav-bar">Notes</a>
+            </Link>
+            <label for="nav-btn">
+              <div className="toggle" >
+                <div className="toggel-btn"></div>
+                <div className="toggel-btn"></div>
+                <div className="toggel-btn"></div>
+              </div>
+            </label>
           </div>
-          <label for="nav-btn">
-            <div class="toggle">
-              <div class="toggel-btn"></div>
-              <div class="toggel-btn"></div>
-              <div class="toggel-btn"></div>
-            </div>
-          </label>
         </Nav>
 
       </Header>
-
       <PageBody>{children}</PageBody>
       </header>
     </React.Fragment>
