3b937eb3350b1b2cb4acacbd18ee558866054d0c
parulsurana <parulsurana28@gmail.com>
2021-03-27 01:42:58 +0530

    Add ChatBot feature

--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../components/navbar.js";
 import Home from "../components/calender/calender.js"
 import Start from "../components/started.js"
+import ChatBot from "../components/ChatBot/ChatBot"
 
 const Homepage =()=> {
   return (
     <>
       <Navbar />
+      <ChatBot />
       <Start/>
       </>
   );
