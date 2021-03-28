d194cd9e754e2ceaef66a965359433620280bca4
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-03-28 15:21:41 +0530

    Footer added

---
+++ periodo/components/Footer/Footer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { Container,Row, Col } from "reactstrap";
+import GithubIcon from "@material-ui/icons/GitHub";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import Link from "next/link"
+export default function Footer() {
+  return (
+    
+    <div>
+      <div className={styles.footer}>
+          <Container>
+        <Row>
+          <Col xs="4" className={styles.one}>
+            <ul>
+              <a href="/">About Us</a>
+              <br />
+              <br />
+              <a
+               className={styles.iconGit}
+                href="https://github.com/Feminine-Divine/periodo"
+                target="_blank"
+              >
+                <GithubIcon
+                className={styles.iconGit}
+                  alt="github"
+                  style={{ width: "65px", height: "37px", color: "white" }}
+                />
+              </a>
+            </ul>
+          </Col>
+          <Col xs="4" className={styles.two}>
+            <ul>
+              <Link href="/contact">Contact Us</Link>
+              <br />
+              <br />
+              <ContactSupportIcon  style={{width:"50px",height:"40px",marginLeft:"25px"}}/>
+            </ul>
+          </Col>
+        </Row>
+        </Container>
+      </div>
+
+      <div className={styles.base}>
+        <p className="text-center" style={{ fontWeight: "100" }}>
+          Made with <FavoriteIcon style={{ color: "#fca9c5" }} />
+          &nbsp;by
+          <a
+            href="https://github.com/Feminine-Divine                                                                               "
+            target="_blank"
+          >
+            <b style={{ color: "#fca9c5", fontWeight: "bold" }}>
+              &nbsp; <u>FeminineDivine</u>
+            </b>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "../components/navbar.js";
 import Home from "../components/calender/calender.js"
 import Start from "../components/started.js"
-
+import Footer from "../components/Footer/Footer.js"
 const Homepage =()=> {
   return (
     <>
       <Navbar />
       <Start/>
+      <Footer/>
       </>
   );
 }
