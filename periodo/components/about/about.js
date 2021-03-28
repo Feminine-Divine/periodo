08d6f437b87779254a7f353668b89327c14d8226
Sukriti-sood <soodsukriti1111@gmail.com>
2021-03-28 07:41:08 +0530

    Made website responsive

--- periodo/components/about/about.js
+++ periodo/components/about/about.js
@@ -8,24 +8,27 @@ import Content from "./aboutstyle.js";
 const about=[
     {
         aboutNumber:"TRACK YOUR CYCLE",
-        aboutContent:"Eliminate those pesky mood swings and stay in the know with our custom calendar."
+        aboutContent:"Eliminate those pesky mood swings and stay in the know with our custom calendar.",
+        imgsrc:"./Medicine.svg"
 
 
     },
     {
       aboutNumber:"CHAT WITH OUR VERY OWN CHATBOT",
       aboutContent:"Tired of Humans? Our friendly neighbourhood chatbot is happy to lend an ear! ",
+      imgsrc:"./Chatbot.svg"            
 
     },
     {
       aboutNumber:"TAKE CONTROL OF YOUR DIET",
       aboutContent:" Your body is a temple. Visit the tips section to be cognescent of what's best for you.",
+      imgsrc:"./Diet.svg"
 
     },
     {
       aboutNumber:"GET CUSTOMISED MEDICINAL ADVICE",
       aboutContent:"No amount of pain is worth it. Find expert advice at your doorstep from women who have gone through the same.",
-
+      imgsrc:'./Calendar.svg'
     }
 ]
 
