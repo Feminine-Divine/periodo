5a0ac6e9a4065956d3266c5d3b168b642f6aac27
Parul Surana <63437071+parulsurana@users.noreply.github.com>
2021-03-29 17:42:15 +0530

    Add ChatBot feature (#147)
    * Add ChatBot feature
    
    * Change the position of ChatBot Button

--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/navbar.js";
 import Home from "../components/calender/calender.js"
 import Start from "../components/started.js"
+import ChatBot from "../components/ChatBot/ChatBot"
 
 const Homepage =()=> {
   return (
     <>
       <Navbar />
       <Start/>
+      <ChatBot />
       </>
   );
 }
