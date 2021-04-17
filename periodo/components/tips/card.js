bb284295566e305b0aa69a16ddc4fd7c3e8736f9
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-04-17 13:53:37 +0530

    tips component added

--- periodo/components/tips/card.js
+++ periodo/components/tips/card.js
@@ -1,39 +1,32 @@
 import React from "react";
 import style from "./cards.module.css";
-import Image from 'next/image'
-const card=({props})=>{
-        const propsList=props.map((prop)=>{
-            return (
-            <div className={style.card}>
-                <div className={style.circle}>
-                <div className={style.img}>
-                            <Image src="/bg.png" layout="fill" />
-                        </div>
-                    <div className={style.h2}>
-                    
-                        <h2 >{prop.tipNumber}</h2>
-                       
-                    </div>
-                </div>
-
-                <div className={style.content}>
-                    
-                    <p className={style.p}>{prop.tipContent} </p>
-                    <a href={prop.link}  className={style.a}>Read more </a>
-                </div>
-                
-            </div>
-        );
-    })
+import Image from "next/image";
+const card = ({ props }) => {
+  const propsList = props.map((prop) => {
     return (
-        <div className={style.section}>
-            <div className={style.body}>
-            
-                <div className={style.container}>
-                    {propsList}
-                </div>                
-            </div>
+      <div className={style.card}>
+        <div className={style.circle}>
+          <div className={style.img}></div>
+          <div className={style.h2}>
+            <h2>{prop.tipNumber}</h2>
+          </div>
         </div>
+
+        <div className={style.content}>
+          <p className={style.p}>{prop.tipContent} </p>
+          <a href={prop.link} className={style.a}>
+            Read more{" "}
+          </a>
+        </div>
+      </div>
     );
-}
+  });
+  return (
+    <div className={style.section}>
+      <div className={style.body}>
+        <div className={style.container}>{propsList}</div>
+      </div>
+    </div>
+  );
+};
 export default card;
