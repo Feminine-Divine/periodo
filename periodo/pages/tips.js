194b4edd28493f7846bcb7dbfe9a3e99b70fe92e
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-04-17 10:27:21 +0530

    tip.js

--- periodo/pages/tips.js
+++ periodo/pages/tips.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Image from "next/image"
 import Tips from "../components/tips/tips";
 const tips =()=>{
     return (
+        <>
         <Tips></Tips>
+        </>
     );
 }
 export default tips;
