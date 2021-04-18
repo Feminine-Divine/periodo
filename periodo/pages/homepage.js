8346d70f12b2a077242e34b12b6ef20300cd2428
Vanshika <vanshikayadavby@gmail.com>
2021-04-18 23:15:00 +0530

    Added Emergency Button. Fixed #78.

--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Navbar from "../components/navbar.js";
-import Start from "../components/started.js";
-import Layout from "../components/Layout";
-import Footer from "../components/Footer/Footer.js";
-import ChatBot from "../components/ChatBot/ChatBot";
-import Emergency from "../components/Emergency/Emergency";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import MailIcon from "@material-ui/icons/Mail";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import styles from '../styles/Home.module.css';
+import Home from "../components/calender/calender.js"
+import Start from "../components/started.js"
+import Footer from "../components/Footer/Footer.js"
+import ChatBot from "../components/ChatBot/ChatBot"
+import Emergency from "../components/Emergency/Emergency"
 
 
 
@@ -28,7 +23,7 @@ const Homepage = () => {
       <Layout />
       <ChatBot />
       <Emergency />
-    </>
+      </>
   );
 };
 
