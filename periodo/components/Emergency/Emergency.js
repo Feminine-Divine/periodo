d917bd8c5ac5e0c0e8eb5b3ad0a36a1e0253f1db
Vanshika <vanshikayadavby@gmail.com>
2021-04-26 15:01:08 +0530

    Added options to restrooms button. Fixed #78

--- periodo/components/Emergency/Emergency.js
+++ periodo/components/Emergency/Emergency.js
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styles from "./emergency.module.css";
 
-
 const App = (props) => {
+  let [showMenu, setShowMenu] = useState(false);
+
+  const startMenu = () => { setShowMenu(true); }
+  const hideMenu = () => { setShowMenu(false); }
+
     return (
-      <>
-      <div className = "emergency"> 
-        <a href="https://www.google.com/maps/search/restrooms+near+me" target="_blank">
-         <button className={styles.btn} >Emergency</button>
-        </a> 
+      <>     
+      <div className = "emergency">
+        <div style ={{display: showMenu ? "" : "none"}}>
+            <a href="https://www.google.com/maps/search/public+toilet+near+me/" target="_blank">
+              <button className = {styles.btn} onClick={() => hideMenu()}>Public</button >
+            </a>
+            <a href="https://www.google.com/maps/search/restaurants+near+me/" target="_blank">
+              <button className = {styles.btn} onClick={() => hideMenu()}>Restaurant</button>
+            </a>
+            <a href="https://www.google.com/maps/search/hospitals+near+me/" target="_blank">
+              <button className = {styles.btn} onClick={() => hideMenu()}>Hospital</button>
+            </a>
+        </div>      
+        {!showMenu 
+            ?  <button className = {styles.btn} onClick={() => startMenu()}>Restrooms</button> 
+            :  <button className={styles.btn} onClick={() => hideMenu()}>Restrooms</button>
+        }
       </div>      
       </>
     )
 }
 
 export default App;
+
