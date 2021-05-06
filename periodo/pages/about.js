7dade07a1c153e170c47ef5d6e76f4a6acadcf34
Vanshika Yadav <65587816+vanshika2906@users.noreply.github.com>
2021-05-06 22:51:08 +0530

    Created Common layout (#257)
    * Added Emergency Button.
    Fixed #78.
    
    * Added options to restrooms button.
    Fixed #78
    
    * Updated Ui for Restroom.
    Fixed #78
    
    * Updated UI for Restroom button.
    Fixed #78
    
    * create-layout.
    Fixed #249

--- periodo/pages/about.js
+++ periodo/pages/about.js
@@ -1,16 +1,17 @@
 import React from "react";
 import About from "../components/about/about";
-import Head from "next/head";
-import Navbar from "../components/navbar";
-import Footer from "../components/Footer/Footer";
+import Head from 'next/head'
+import Layout from '../components/Layout';
+
 const about = () => {
   return (
     <>
+      <Layout>
       <About></About>
       <Head>
-        <title>About</title>
+         <title>About</title>
       </Head>
-      <Footer />
+      </Layout>
     </>
   );
 };
