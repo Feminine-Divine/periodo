0f40338d47fde664e7c33c5f8d56099b2f778570
kumarishalini6 <kumarishalu1206@gmail.com>
2021-05-16 22:24:43 -0800

    some changes

--- periodo/pages/homepage.js
+++ periodo/pages/homepage.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Navbar from "../components/navbar.js";
-import Home from "../components/calender/calender.js";
 import Start from "../components/started.js";
 import Footer from "../components/Footer/Footer.js";
 import ChatBot from "../components/ChatBot/ChatBot";
 import Emergency from "../components/Emergency/Emergency";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import MailIcon from "@material-ui/icons/Mail";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import styles from '../styles/Home.module.css';
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
+      <br></br>
+        <div className={styles.socialIcons}>
+            <a data-testid="github" href="https://github.com/Feminine-Divine/periodo"><GitHubIcon className={styles.icons} /></a>
+            <a data-testid="mail" href="mailto:femininedivine591@gmail.com"><MailIcon className={styles.icons} /></a>
+            <a data-testid="linkedin" href="https://www.linkedin.com/company/feminine-divine/"><LinkedInIcon className={styles.icons} /></a>
+            <a data-testid="instagram" href=""><InstagramIcon className={styles.icons} /></a>
+        </div>
       <Start />
       <Footer />
       <ChatBot />
