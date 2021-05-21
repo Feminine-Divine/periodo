69d9b5e17cb073b556ddb98164930f9d805baca5
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-21 21:21:32 +0530

    integrate google maps using react

---
+++ periodo/components/Gynae/GoogleMap.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{'Jeypore'}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+ 
+export default GoogleApiWrapper({
+  apiKey: ('API_KEY')
+})(MapContainer)
