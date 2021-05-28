5fa9a44fb4872fba4b7b8c74dfb353bcbc43931b
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-29 03:16:58 +0530

    updated contact form to new styles!

--- periodo/components/Footer/ContactUs.js
+++ periodo/components/Footer/ContactUs.js
@@ -1,129 +1,88 @@
 import emailjs from "emailjs-com";
-import React from 'react';
-import {TextField} from '@material-ui/core';
-import {Card, CardHeader, CardContent} from '@material-ui/core';
-import EmailIcon from '@material-ui/icons/Email';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import SubjectIcon from '@material-ui/icons/Subject';
-import PersonIcon from '@material-ui/icons/Person';
-import { TextareaAutosize } from '@material-ui/core';
-import {Paper, Grid} from '@material-ui/core';
+import React from "react";
+import { TextField, Button, Container } from "@material-ui/core";
+import { Card, CardHeader, CardContent } from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import PersonIcon from "@material-ui/icons/Person";
+import { TextareaAutosize } from "@material-ui/core";
+import { Paper, Grid } from "@material-ui/core";
+import { Row, Col } from "reactstrap";
+import styles from "./ContactUs.module.css";
+import 'font-awesome/css/font-awesome.min.css';
 
 
 export default function ContactUs() {
 
     function sendEmail(e) {
         e.preventDefault();
-     
+
         // NOTE: make sure to make your own setup and also make .env configurations!
 
-    emailjs.sendForm(
-        process.env.NEXT_PUBLIC_CONTACT_FORM_SERVICE_ID, 
-        process.env.NEXT_PUBLIC_CONTACT_FORM_TEMPLATE_ID, 
-        e.target,
-        process.env.NEXT_PUBLIC_CONTACT_FORM_USER_ID
+        emailjs.sendForm(
+            process.env.NEXT_PUBLIC_CONTACT_FORM_SERVICE_ID,
+            process.env.NEXT_PUBLIC_CONTACT_FORM_TEMPLATE_ID,
+            e.target,
+            process.env.NEXT_PUBLIC_CONTACT_FORM_USER_ID
         )
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
         e.target.reset()
     }
 
-    return(
-        <div>
-            <Paper style={{alignItems:"center", margin:"0 19%", background:"#960435"}}>
-        <Card style={{backgroundColor: '#960435',
-                      color: 'white'}}>
-          <CardHeader title="Talk with us (beta)"/>
-        </Card>
-        <CardContent>
-            <form onSubmit={sendEmail}>
-                        <Grid item md={12} sm={12} xs={12} lg={12}>
-                            <TextField
-                            margin="dense"
-                            variant="filled"
-                            name="name"
-                            label="Name"
-                            color="secondary"
-                            autoComplete="off"
-                            style={{width:"100%", background:"#fad2e3"}}
-                            required
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position="end">
-                                    <PersonIcon />
-                                </InputAdornment>
-                                ),
-                            }}
-                            />
-                        </Grid>
-                        <Grid style={{alignItems:"center"}} item md={12} sm={12} xs={12} lg={12}>
-                            <TextField
-                            margin="dense"
-                            label="Email"
-                            variant="filled"
-                            color="secondary"
-                            name="email"
-                            autoComplete="off"
-                            required
-                            style={{width: '100%', background:"#fad2e3"}}
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position="end">
-                                    <EmailIcon />
-                                </InputAdornment>
-                                ),
-                            }}
-                            />
-                        </Grid>
-                        <Grid item md={12} sm={12} xs={12} lg={12}>
-                            <TextField
-                            margin="dense"
-                            variant="filled"
-                            name="subject"
-                            label="subject"
-                            color="secondary"
-                            autoComplete="off"
-                            style={{width:"100%", background:"#fad2e3"}}
-                            required
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position="end">
-                                    <SubjectIcon />
-                                </InputAdornment>
-                                ),
-                            }}
-                            />
-                        </Grid>
-                        <Grid style={{alignItems:"center"}} item md={12} sm={12} xs={12} lg={12}>
-                            <TextareaAutosize
-                                rowsMax={4}
-                                aria-label="maximum height"
-                                variant="filled"
-                                name="message"
-                                placeholder="Message"
-                                style={{width: '100%', 
-                                        background:"#e9b8cc", 
-                                        maxHeight:"90px", 
-                                        minHeight:"90px", 
-                                        overflowY: "scroll",
-                                        fontSize:"17px",
-                                        outline: "none",
-                                        padding:"10px",
-                                        marginTop:"10px"
-                                    }}
-                            />
-                        </Grid>
-                        <div>
-                            <input type="submit" value="Send Message" 
-                            style={{background:"black", color:"white", padding:"10px", border:"2px black solid", borderRadius:"10px", marginTop:"10px" }}>
-                            </input>
+    return (
+        <div className={styles.contactus_body}>
+            <div className={styles.form_wrapper}>
+                <div className={styles.form_container}>
+                    <div className={styles.title_container}>
+                        <h2>Talk to us <span id={styles.touch}>Beta</span></h2>
+                        <h3>Contact us</h3>
+                    </div>
+                    <form onSubmit={sendEmail}>
+                        <div className={styles.row_clearfix}>
+                            <div className={styles.col_half}>
+                                <label className={styles.input_label}>First name</label>
+                                <div className={styles.input_field}> <span><i aria-hidden="true" className="fa fa-user"></i></span>
+                                    <input className={styles.inputN} type="text" name="first_name" placeholder="John" required />
+                                </div>
+                            </div>
+                            <div className={styles.col_half}>
+                                <label className={styles.input_label}>Last name</label>
+                                <div className={styles.input_field}> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                                    <input className={styles.inputN} type="text" name="last_name" placeholder="Doe" />
+                                </div>
+                            </div>
                         </div>
-                </form>
-                </CardContent>
-                </Paper>
+                        <div className={styles.row_clearfix}>
+                            <div className={styles.col_half}>
+                                <label className={styles.input_label}>Email</label>
+                                <div className={styles.input_field}> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
+                                    <input className={styles.inputN} type="email" name="email" placeholder="johndoe@gmail.com" required />
+                                </div>
+                            </div>
+                            <div className={styles.col_half}>
+                                <label className={styles.input_label}>Phone</label>
+                                <div className={styles.input_field}> <span><i aria-hidden="true" class="fa fa-phone"></i></span>
+                                    <input className={styles.inputN} type="tel" name="phone" placeholder="Phone no" pattern="[0-9]{10}" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row_clearfix}>
+                            <div>
+                                <label className={styles.input_label}>Comments</label>
+                                <div className={styles.textarea_field}> <span><i aria-hidden="true" class="fa fa-comment"></i></span>
+                                    <input type="text" className={styles.inputtextarea} name="comments" />
+                                </div>
+                            </div>
+                        </div>
+                        <input className={styles.inputN} type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+export default ContactUs;
