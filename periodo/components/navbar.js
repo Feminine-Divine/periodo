b934f45fea197502cb1e612f1881e76f06df3abe
Kumari Shalini <69043944+kumarishalini6@users.noreply.github.com>
2021-05-27 23:58:42 -0800

    Social campaign (#378)
    * social camp
    
    * social campaign

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -13,6 +13,7 @@ import {
   faSignOutAlt,
   faSignInAlt,
 } from "@fortawesome/free-solid-svg-icons";
+import CollectionsIcon from '@material-ui/icons/Collections';
 import classes from "./navbar.component.style.js";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -194,6 +195,14 @@ const Navbar = () => {
                   </ListItemText>
             </ListItem>
           )}
+          <ListItem button component="a" href="/social">
+                <ListItemIcon>
+                  <FontAwesomeIcon icon={CollectionsIcon} />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography variant="h6">Social Campings</Typography>
+                </ListItemText>
+              </ListItem>
           <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
           <br></br><br></br><br></br><br></br><br></br>
         </List>
