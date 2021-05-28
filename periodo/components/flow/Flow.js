bf640e753093918d9ca6b83da1c9ae1c1fe93f5a
Subhangi Choudhary <53938490+subhangi2731@users.noreply.github.com>
2021-05-28 17:23:13 +0530

    prior or delay graphs (#377)
    Co-authored-by: Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>

--- periodo/components/flow/Flow.js
+++ periodo/components/flow/Flow.js
@@ -2,7 +2,9 @@ import React from "react";
 import BeautyStars from "beauty-stars";
 import style from "./Flow.module.css";
 import Grid from "@material-ui/core/Grid";
-import { VictoryChart, VictoryLine, VictoryTheme, VictoryContainer } from "victory";
+
+import { VictoryChart, VictoryLine,VictoryAxis,VictoryLabel, VictoryTheme, VictoryContainer,VictoryStack,VictoryBar } from "victory";
+
 
 export default class App extends React.Component {
   state = {
@@ -156,7 +158,8 @@ export default class App extends React.Component {
             </div>
           </div>
         </Grid>
-        <VictoryChart
+        {/* <VictoryChart
+
          containerComponent={<VictoryContainer responsive={false} style={{marginTop: 300}}/>}
         height={600} width={800}
             theme={VictoryTheme.material}
@@ -177,7 +180,46 @@ export default class App extends React.Component {
                 { x: 'HairLoss', y: this.state.value8 },
               ]}
             />
-          </VictoryChart>        
+
+          </VictoryChart>         */}
+
+          
+
+          <VictoryChart
+         containerComponent={<VictoryContainer responsive={false} style={{marginTop: 300}}/>} // Data from previous months can be mapped from an array and plotted
+        height={600} width={800}
+            theme={VictoryTheme.material}
+          >
+          <VictoryStack
+              colorScale={["tomato", "orange", "gold","green","pink","blue","black","red"]}
+            >
+              <VictoryBar
+                data={[{x: "January", y: this.state.value1}, {x: "February", y: this.state.value1}, {x: "March", y: this.state.value1},{x: "April", y: this.state.value1}, {x: "May", y: this.state.value1}, {x: "June", y: this.state.value1}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value2}, {x: "February", y: this.state.value2}, {x: "March", y: this.state.value2},{x: "April", y: this.state.value2}, {x: "May", y: this.state.value2}, {x: "June", y: this.state.value2}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value3}, {x: "February", y: this.state.value3}, {x: "March", y: this.state.value3},{x: "April", y: this.state.value3}, {x: "May", y: this.state.value3}, {x: "June", y: this.state.value3}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value4}, {x: "February", y: this.state.value4}, {x: "March", y: this.state.value4},{x: "April", y: this.state.value4}, {x: "May", y: this.state.value4}, {x: "June", y: this.state.value4}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value5}, {x: "February", y: this.state.value5}, {x: "March", y: this.state.value5},{x: "April", y: this.state.value5}, {x: "May", y: this.state.value5}, {x: "June", y: this.state.value5}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value6}, {x: "February", y: this.state.value6}, {x: "March", y: this.state.value6},{x: "April", y: this.state.value6}, {x: "May", y: this.state.value6}, {x: "June", y: this.state.value6}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value7}, {x: "February", y: this.state.value7}, {x: "March", y: this.state.value7},{x: "April", y: this.state.value7}, {x: "May", y: this.state.value7}, {x: "June", y: this.state.value7}]}
+              />
+              <VictoryBar
+                data={[{x: "January", y: this.state.value8}, {x: "February", y: this.state.value8}, {x: "March", y: this.state.value8},{x: "April", y: this.state.value8}, {x: "May", y: this.state.value8}, {x: "June", y: this.state.value8}]}
+              />
+            </VictoryStack>
+            </VictoryChart>  
+
       </div>
     );
   }
