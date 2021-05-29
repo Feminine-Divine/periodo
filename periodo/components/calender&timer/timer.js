55f5301fcf98d026a9c6682d7f0a10cadc1bb2d6
kumarishalini6 <kumarishalu1206@gmail.com>
2021-05-28 18:52:05 -0800

    removing commit line

--- periodo/components/calender&timer/timer.js
+++ periodo/components/calender&timer/timer.js
@@ -14,43 +14,7 @@ export default function timer() {
     }
 
     setInterval(updateTime,1000)
- /*   const [timerDays, setTimerDays] = useState('00');
-    const [timerHours, setTimerHours] = useState('00');
-    const [timerMinutes, setTimerMintues] = useState('00');
-    const [timerSeconds, setTimerSeconds] = useState('00');
-
-    let interval = useRef();
-
-    const startsTimer = () => {
-        const countdownDate = new Date('May 28, 2021 00:00:00').getTime();
-
-        interval = setInterval(() => {
-            const now = new Date().getTime();
-            const distance = countdownDate - now;
-
-            const days = (distance / (1000*60*60*24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-            const minutes = Math.floor((distance % (1000 *60 * 60))/ (1000 * 60));
-            const seconds = Math.floor((distance % (1000 *60)) / (1000));
-
-            if(distance < 0){
-                clearInterval(interval.current)
-            }else{
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMintues(minutes);
-                setTimerSeconds(seconds);
-            }
-        }, 1000);
-    }
-
-    useEffect(() => {
-        startsTimer();
-        return () =>{
-            clearInterval(interval.current)
-        }
-    })
-    */
+ 
     return (
         <div style={{marginTop:"50px"}}>
         <section className={style.timer_container}>
