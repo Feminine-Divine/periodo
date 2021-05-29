fb2ab729ab9e6f7d471c1c719c817124322748f4
Kumari Shalini <69043944+kumarishalini6@users.noreply.github.com>
2021-05-29 00:17:58 -0800

    FAQs in navbar (#373)
    * Update data.js
    
    * Update data.js
    
    * faqs in navbar
    
    * added
    
    * added
    
    * all data in one file
    
    Co-authored-by: Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
    Co-authored-by: Khushi <dpskhu13108@gmail.com>

---
+++ periodo/components/FAQs/questions.js
@@ -0,0 +1,185 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+import style from '../FAQs/questions.module.css';
+
+
+const FAQSection = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+position: relative;
+height: 100vh;
+background: #fff;
+`;
+const Container = styled.div`
+position:absolute;
+margin-top: 50%;
+box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+const Wrap = styled.div``;
+
+const Dropdown = styled.div`
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 3px solid #00ffb9;
+  border-top: 3px solid #00ffb9;
+`;
+
+export default function questions() {
+  let data = [
+    {
+      question: 'Why is my period late?',
+      answer: 'There are several reasons why your period might be late.First of all: you might be pregnant. If you think this could be the case, you can take a home pregnancy test or see your doctor to check.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Period pain comes with the territory?',
+      answer: 'Abdominal cramps, headaches and back pain, breast tenderness, mood swings or nausea, for many women make periods a regular misery. But you should certainly not suffer severe discomfort in silence. Consult your doctor or healthcare provider so they can investigate possible causes and ask about ways you can alleviate pain relating to your menstrual cycle. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Can I get pregnant during my period?',
+      answer: 'During monthly bleeding the chances of pregnancy are low but not zero. Bleeding itself does not prevent pregnancy, and it does not promote pregnancy, either. In the first several days of monthly bleeding, the chances of pregnancy are lowest. As the days pass, the chances of pregnancy increase, whether or not she is still bleeding. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'How can I know my fertile days?',
+      answer: 'Ovulation happens about 14 days before your period starts. Let’s say your average menstrual cycle is 28 days, you ovulate around day 14, and your most fertile days are days 12, 13 and 14. If your average menstrual cycle is 35 days ovulation happens around day 21 and your most fertile days are days 19, 20 and 21. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'How long is the average period length?',
+      answer: 'The time from the first sign of blood to the last is usually in the 3-to-5-day range.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Can I get pregnant if I’ve never had my period?',
+      answer: 'We have to admit it can be a bit misleading, but yes, you can get pregnant before your first period and here is why: getting pregnant is related to ovulation. Ovulation means that your ovaries release an egg which is ready to be fertilized by male sperm. Your first ovulation occurs around 2 weeks before your first menstruation. So, it is possible to become pregnant. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Can I get pregnant a day after my period?',
+      answer: 'Its unlikely that youll get pregnant just after your period, although it can happen. Sperm can sometimes survive in the body for up to 5 days after having sex. This means it may be possible to get pregnant soon after your period finishes if you ovulate early, especially if your menstrual cycle is naturally short.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What happens during ovulation?',
+      answer: 'There are four phases in the female cycle: menstruation, the post-menstrual phase, ovulation, and finally the pre-menstrual phase. In the ovulation phase, hormones stimulate the ovary to release a mature egg. During ovulation, the mature egg travels down the fallopian tube from the ovary into the uterus.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Can sperm survive in period blood?',
+      answer: 'No way - period blood cant flush away sperm. Please always use a contraceptive method to avoid unintended pregnancies.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What causes hormonal imbalance?',
+      answer: 'A hormonal imbalance means that women or girls have too much or too little of a certain hormone (like estrogen and progesterone).Estrogen, progesterone, testosterone, thyroxin, insulin and cortisol levels can be detected in the blood through blood tests. Please talk to your healthcare provider for individual medical advice.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What do irregular periods mean?',
+      answer: 'Many women and girls suffer from irregular bleedings during their monthly menstrual cycles. Your period is irregular if it comes more frequently than 21 days, or you go 35 days or more between periods, the cycle length varies greatly. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'How to treat irregular periods?',
+      answer: 'Indeed, if a menstrual cycle is regular, there is usually no reason for concern. Having a lower than average menstrual cycle length does not automatically mean that there is something wrong. For irregular periods there usually is also no immediate reason to consult your healthcare provider but if you are concerned, please do so. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Is it normal to have no pain during periods?',
+      answer: 'Not every woman or girl experiences period pain. For some, menstruation is entirely painless and problem-free. Be happy if you are one of them!',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Why are my period cramps so painful?',
+      answer: 'Painful period cramps could be caused by heavy menstrual bleeding, which often affects women or girls to take part in everyday life around their periods, or they could be caused by endometriosis. In this painful condition, the cells or tissue lining the uterus, grow outside the uterus. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'How to reduce period cramps?',
+      answer: 'To reduce period cramps and pain you can try the following things:- Over-the-counter pain medicine - Putting a heating pad on your belly or lower back',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What are the reasons for less bleeding during periods?',
+      answer: 'Lighter periods than usual do not usually cause concern. People often find their menstrual flow varies from month-to-month, and some months are naturally lighter than others. The following factors can cause periods to be lighter than usual: a lack of ovulation, being underweight, stress, pregnancy or a medical condition.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Is it normal to have a period twice in a month?',
+      answer: 'Yes, it’s possible. Some months, your cycle may last for more or fewer days than the previous month, or it may start earlier or later than before. Sometimes, you may even have two periods in a single month. If you usually have a regular cycle, a change in your cycle could indicate a medical condition. ',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What does it mean when your period lasts more than 7 days?',
+      answer: 'Well, if you have bleeding that lasts longer than 7 days per period or is so heavy that you have to change your pad or tampon nearly every hour, you need to talk to your doctor.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What are safe days during menstrual cycle?',
+      answer: 'Wait a minute - theres no "safe" time of the month when you can have sex without contraception and not risk becoming pregnant. You can also get pregnant if you’ve never had a period before, during your first period, or after the first time you have sex. You’re most fertile at the time of ovulation, which usually occurs 12 to 14 days before your next period starts.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'What is PMS? Is it real?',
+      answer: 'Yes, PMS is not a myth! Eight out of ten women do experience some discomfort shortly before their monthly period sets in. These symptoms are caused by the hormonal changes of the cycle and for many women they are so severe that they impact on their daily lives – both in personal and working aspects.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    },
+    {
+      question: 'Are cycle-related symptoms common?',
+      answer: 'You are not alone! Cycle-related symptoms are experienced by about eight out of ten women at some stage during their fertile lives. If you complain about disturbances such as excessive cravings, you may experience symptoms that differ from those your sister or your best friend is suffering from.',
+      link:'https://www.your-life.com/en/your-questions/answers-to-your-top-menstruation-and-period-questions/#:~:text=to%20your%20doctor.-,How%20long%20is%20the%20average%20period%20length%3F,late'
+    }
+  ];
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
+      return (
+        <>
+        <div className={style.intro}>
+        <h1>| FREQUENTLY ASKED QUESTIONS (FAQs) |</h1>
+        </div>
+        <IconContext.Provider value={{color:'#03ecae', size:"55px"}}>
+            <FAQSection className={style.FAQSection}>
+                <Container className={style.container}>
+                    {data.map((item, index) => {
+                        return (
+                            <> 
+                            <Wrap onClick={() => toggle(index)} key={index} className={style.wrap}>
+                                <h2 className={style.h1} style={{marginLeft:"20px"}}>{item.question}</h2>
+                                <span className={style.span}>{clicked === index ? <FiMinus/> : <FiPlus />}</span>
+                            </Wrap>
+                            {clicked === index ? (
+                              <Dropdown>
+                              <p className={style.p} style={{marginLeft:"20px", marginRight:"20px"}}>{ item.answer}</p>
+                              <a href={item.link} className={style.a} target="_blank">
+                                Read more
+                              </a>
+                              <br></br><br></br>
+                              </Dropdown>
+                            ):null }
+                            
+                            </>
+                        )
+                    })}
+                </Container>
+            </FAQSection>
+        </IconContext.Provider>
+        </>
+    )
+}
