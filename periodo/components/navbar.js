4efc336c05ec28a82aae8293b6bea5030e509dff
garvitgupta13 <garvitgupta2001@gmail.com>
2021-05-31 00:25:23 +0530

    change the navbar colour

--- periodo/components/navbar.js
+++ periodo/components/navbar.js
@@ -12,9 +12,9 @@ import {
   faStar,
   faSignOutAlt,
   faSignInAlt,
-  faQuestion
+  faQuestion,
 } from "@fortawesome/free-solid-svg-icons";
-import CollectionsIcon from '@material-ui/icons/Collections';
+import CollectionsIcon from "@material-ui/icons/Collections";
 import classes from "./navbar.component.style.js";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -57,16 +57,16 @@ const Navbar = () => {
   };
 
   return (
-    <div >
+    <div>
       <CssBaseline />
       <AppBar
         position="fixed"
-        style={{ backgroundColor: "rgb(255, 192, 203)" }}
+        style={{ backgroundColor: "rgb(32,30,30)" }}
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
       >
-        <Toolbar >
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -90,7 +90,10 @@ const Navbar = () => {
           paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.drawerHeader} style={{ backgroundColor: "rgb(255, 182, 193)" }}>
+        <div
+          className={classes.drawerHeader}
+          style={{ backgroundColor: "rgb(255, 182, 193)" }}
+        >
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === "ltr" ? (
               <ChevronLeftIcon />
@@ -101,17 +104,20 @@ const Navbar = () => {
         </div>
         <Divider />
 
-<ThemeProvider theme={nav}>
-        <div height="30px">
-        <List className={classes.root} style={{ backgroundColor: "rgb(255, 182, 193)" }}>
-          <ListItem button component="a" href="/homepage">
-            <ListItemIcon>
-              <FontAwesomeIcon icon={faHome} />
-            </ListItemIcon>
-             <ListItemText>
+        <ThemeProvider theme={nav}>
+          <div height="30px">
+            <List
+              className={classes.root}
+              style={{ backgroundColor: "rgb(255, 182, 193)" }}
+            >
+              <ListItem button component="a" href="/homepage">
+                <ListItemIcon>
+                  <FontAwesomeIcon icon={faHome} />
+                </ListItemIcon>
+                <ListItemText>
                   <Typography variant="h6">Home</Typography>
                 </ListItemText>
-          </ListItem>
+              </ListItem>
 
               <ListItem button component="a" href="/about">
                 <ListItemIcon>
@@ -149,7 +155,6 @@ const Navbar = () => {
                 </ListItemText>
               </ListItem>
 
-
               <ListItem button component="a" href="/calender">
                 <ListItemIcon>
                   <FontAwesomeIcon icon={faCalendarAlt} />
@@ -169,7 +174,7 @@ const Navbar = () => {
               </ListItem>
               <ListItem button component="a" href="/questions">
                 <ListItemIcon>
-                  <FontAwesomeIcon icon={  faQuestion} />
+                  <FontAwesomeIcon icon={faQuestion} />
                 </ListItemIcon>
                 <ListItemText>
                   <Typography variant="h6">FAQs</Typography>
@@ -185,26 +190,26 @@ const Navbar = () => {
                 </ListItemText>
               </ListItem>
 
-          {user ? (
-            <ListItem button component="a" href="/api/auth/logout">
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </ListItemIcon>
-            <ListItemText>
+              {user ? (
+                <ListItem button component="a" href="/api/auth/logout">
+                  <ListItemIcon>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                  </ListItemIcon>
+                  <ListItemText>
                     <Typography variant="h6">Logout</Typography>
                   </ListItemText>
-            </ListItem>
-          ) : (
-            <ListItem button component="a" href="/api/auth/login">
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faSignInAlt} />
-              </ListItemIcon>
-               <ListItemText>
+                </ListItem>
+              ) : (
+                <ListItem button component="a" href="/api/auth/login">
+                  <ListItemIcon>
+                    <FontAwesomeIcon icon={faSignInAlt} />
+                  </ListItemIcon>
+                  <ListItemText>
                     <Typography variant="h6">Login</Typography>
                   </ListItemText>
-            </ListItem>
-          )}
-          <ListItem button component="a" href="/social">
+                </ListItem>
+              )}
+              <ListItem button component="a" href="/social">
                 <ListItemIcon>
                   <FontAwesomeIcon icon={CollectionsIcon} />
                 </ListItemIcon>
@@ -212,14 +217,24 @@ const Navbar = () => {
                   <Typography variant="h6">Social Campings</Typography>
                 </ListItemText>
               </ListItem>
-          <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-          <br></br><br></br><br></br><br></br><br></br>
-        </List>
-        </div>
- </ThemeProvider>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </List>
+          </div>
+        </ThemeProvider>
       </Drawer>
     </div>
   );
 };
 export default Navbar;
-
